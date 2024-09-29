f621ed53a993d928156f19db431d24e69ba8c491
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-09-29 16:16:57 +0330

    add app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Form } from "./components/Form";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="container max-w-full w-full ">
+      <Navbar />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
